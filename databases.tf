b046136a6efff5c790824cacbe87223f1c2b3dfb
Matt Boyd <mjb141@gmail.com>
2020-06-23 17:11:50 +0100

    Updating for SQLServer

--- databases.tf
+++ databases.tf
@@ -1,4 +1,28 @@
 # Azure SQL
 
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.resource_prefix}-storage"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_sql_server" "SQL" {
+  name                         = "${var.resource_prefix}-SQL"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  version                      = "12.0"
+  administrator_login          = "mradministrator"
+  administrator_login_password = "thisIsDog11"
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.storage.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.storage.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
+}
+
 # Azure Container Instance : MongoDB
 
