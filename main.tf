261a658b5fa3f3afe0bac0abd399e1b92bc64d1f
Matt Boyd <mjb141@gmail.com>
2020-06-23 09:24:57 +0100

    Adding back variable files

--- main.tf
+++ main.tf
@@ -4,36 +4,27 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "Azure-Eats-RG"
-  location = "northeurope"
+  name = "{$var.resource_prefix}-RG"
+  location = var.location
 }
 
 resource "azurerm_app_service_plan" "asp" {
-  name = "Azure-Eats-ASP"
+  name = "{$var.resource_prefix}-ASP"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = var.asp_tier
+    size = var.asp_size
   }
 }
 
 resource "azurerm_app_service" "webapp" {
-  name = "Azure-Eats-WebApp"
+  name = "{$var.resource_prefix}-WebApp"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
 
-  app_settings = {
-    WEBSITE_NODE_DEFAULT_VERSION = "10.15.2"
-    ApiUrl = ""
-    ApiUrlShoppingCart = ""
-    MongoConnectionString = ""
-    SqlConnectionString = ""
-    productImagesUrl = "https://raw.githubusercontent.com/microsoft/TailwindTraders-Backend/master/Deploy/tailwindtraders-images/product-detail"
-    Personalizer_ApiKey = ""
-    Personalizer_Endpoint = ""
-    }
+  app_settings = var.app_settings
 
   }
