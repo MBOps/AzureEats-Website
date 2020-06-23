5fc0868c154835690135dbe8c490bca456cb0f01
Matt Boyd <mjb141@gmail.com>
2020-06-23 08:46:07 +0100

    Add local terraform for testing

--- main.tf
+++ main.tf
@@ -3,6 +3,31 @@ provider "azurerm" {
   features {}
 }
 
+variable "resource_prefix" {
+  type        = string
+  description = "Prefix for Resources"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Resources"
+}
+
+variable "asp_tier" {
+  type        = string
+  description = "Tier of App Service Plan"
+}
+
+variable "asp_size" {
+  type        = string
+  description = "Size of App Service Plan"
+}
+
+variable "app_settings" {
+  type        = map(string)
+  description = "object containing app settings for the App Service"
+}
+
 resource "azurerm_resource_group" "rg" {
   name = "${var.resource_prefix}-RG"
   location = var.location
