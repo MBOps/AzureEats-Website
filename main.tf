8a13433960f8971ed88fea6164dc1a556afff6c5
Matt Boyd <mjb141@gmail.com>
2020-06-23 09:55:38 +0100

    Removed all variables from main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "{$var.resource_prefix}-RG"
-  location = var.location
+  name = "Azure-Eats-RG"
+  location = "northeurope"
 }
 
 resource "azurerm_app_service_plan" "asp" {
-  name = "{$var.resource_prefix}-ASP"
+  name = "Azure-Eats-ASP"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -20,7 +20,7 @@ resource "azurerm_app_service_plan" "asp" {
 }
 
 resource "azurerm_app_service" "webapp" {
-  name = "{$var.resource_prefix}-WebApp"
+  name = "Azure-Eats-WebApp"
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
