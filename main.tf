df9f7fd24ea94befc994f663ce58728f3b13c3e4
Matt Boyd <mjb141@gmail.com>
2020-06-23 11:13:16 +0100

    adding TFState storage

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "GitHubActions"
+    storage_account_name = "githubactionsmbops"
+    container_name       = "tfstate"
+    key                  = "challenge-1.terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   version = "~>2.0"
   features {}
