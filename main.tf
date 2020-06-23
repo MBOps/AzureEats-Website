163023b4c61dd460a109d76b6d9a843785ec58b6
Matt Boyd <mjb141@gmail.com>
2020-06-23 15:38:00 +0100

    Testing

--- main.tf
+++ main.tf
@@ -29,20 +29,20 @@ resource "azurerm_app_service_plan" "asp" {
 }
 
 resource "azurerm_app_service" "webapp" {
-  name = "Azure-Eats-WebApp"
-  location = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+    name = "Azure-Eats-WebApp"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    app_service_plan_id = azurerm_app_service_plan.asp.id
 
-  app_settings = {
-    WEBSITE_NODE_DEFAULT_VERSION = "10.15.2"
-    ApiUrl = ""
-    ApiUrlShoppingCart = ""
-    MongoConnectionString = ""
-    SqlConnectionString = ""
-    productImagesUrl = "https://raw.githubusercontent.com/microsoft/TailwindTraders-Backend/master/Deploy/tailwindtraders-images/product-detail"
-    Personalizer_ApiKey = ""
-    Personalizer_Endpoint = ""
-}
+    app_settings = {
+        WEBSITE_NODE_DEFAULT_VERSION = "10.15.2"
+        ApiUrl = ""
+        ApiUrlShoppingCart = ""
+        MongoConnectionString = ""
+        SqlConnectionString = ""
+        productImagesUrl = "https://raw.githubusercontent.com/microsoft/TailwindTraders-Backend/master/Deploy/tailwindtraders-images/product-detail"
+        Personalizer_ApiKey = ""
+        Personalizer_Endpoint = ""
+    }
 
   }
