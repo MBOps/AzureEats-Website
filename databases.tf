dbda7539da6100a41d13ef96bffc85664a4afa7a
Matt Boyd <mjb141@gmail.com>
2020-06-23 17:29:16 +0100

    Test

--- databases.tf
+++ databases.tf
@@ -31,7 +31,7 @@ resource "azurerm_container_group" "ACI" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   ip_address_type     = "public"
-  dns_name_label      = "aci-label"
+  dns_name_label      = "mongodb"
   os_type             = "Linux"
   
   container {
